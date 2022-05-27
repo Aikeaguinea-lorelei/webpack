b93808f22361f4dd9555469a6755a12faefe0a2b
aikea <2656611469@qq.com>
2022-05-27 12:10:49 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path =require('path')   // 添加转译之后文件的保存路径
 
 module.exports = {
@@ -5,6 +6,18 @@ module.exports = {
     entry:'./src/index.js',  // 输入需要被转移的文件
     output:{
         path:path.resolve(__dirname,'dist'),   // 路径是dist
-        filename:'[name].[contenthash].js'  // 转译之后名字是[name].[contenthash].js
-    }
+        filename:'index.[contenthash].js',  // 转译之后名字是[name].[contenthash].js
+    },
+    plugins:[new HtmlWebpackPlugin({
+        title:'My App',
+        template:'src/assets/index.html'
+    })],
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }   
 }
