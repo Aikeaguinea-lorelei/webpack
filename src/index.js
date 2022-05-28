b4cf36a165f2498813eaaba514fb6030427b6cab
aikea <2656611469@qq.com>
2022-05-28 16:52:05 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import x from './x.js'  // 引入当前目录的x.js
-console.log(x)
+import png from '././assets/1.png'  // 从目录中引入
+
+const div=document.getElementById('app')
+
+// 部署到HTML
+div.innerHTML=` 
+    <img src="${png}">
+`
+
+// 懒加载: 当点击按钮的时候 再加载
+const button=document.createElement('button')
+button.innerText='懒加载'
+button.onclick=()=>{
+    const promise=import('./lazy')
+    // 监听这个引入的lazy
+    promise.then(
+        // 成功的话,就...,失败的话,就...
+        (module)=>{
+            module.default() // 模块的默认值
+        },()=>{
+            console.log('模块加载错误')
+        }
+    )
+}
+
+div.appendChild(button)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,33 +1,45 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path =require('path')   // 添加转译之后文件的保存路径
+const path = require('path')   // 添加转译之后文件的保存路径
 
 module.exports = {
-    entry:'./src/index.js',  // 输入需要被转移的文件
-    output:{
-        path:path.resolve(__dirname,'dist'),   // 路径是dist
-        filename:'index.[contenthash].js',  // 转译之后名字是[name].[contenthash].js
+    entry: './src/index.js',  // 输入需要被转移的文件
+    output: {
+        path: path.resolve(__dirname, 'dist'),   // 路径是dist
+        filename: 'index.[contenthash].js',  // 转译之后名字是[name].[contenthash].js
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-        title:'My App',
-        template:'src/assets/index.html'
-    }),
+            title: 'My App',
+            template: 'src/assets/index.html'
+        }),
     ],
     module: {
         rules: [
             {
+                test: /\.(png|svg|jpg|gif)$/,// file-loader作用:把文件变成文件路径
+                use: ["file-loader"]
+              },
+              {
+                test: /\.styl$/,
+                loader: ["style-loader", "css-loader", "stylus-loader"]
+              },// stylus-loader把stylus代码变为css代码,css-loader把css代码变为JS字符串,style-loader把JS字符串变为style标签
+              {
                 test: /\.less$/,
-                loader:['less-loader','css-loader','style-loader'] 
-            },// less-loader把less语法转成css语法,css-loader把css变成JS的字符串,style-loader把字符串变成style标签放进head
-            {
-            test: /\.scss$/i,
-            use: [
-                "style-loader",  // 在开发环境使用 style-loader
-                "css-loader",
-                {
-                loader: "sass-loader",// sass-loader要使用dart-sass
-                options:{implementation:require('dart-sass')}
-            }],
-        }]
+                loader: ["style-loader", "css-loader", "less-loader"]
+              },
+              {
+                test: /\.scss$/i,
+                use: [
+                  "style-loader",
+                  "css-loader",
+                  {
+                    loader: "sass-loader",
+                    options: {
+                      implementation: require("dart-sass")
+                    }
+                  }
+                ]
+              }
+        ]
     },
 }
